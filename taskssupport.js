f8169f68cead3a3eb38d51141f6cf45bbec5a204
Jordan Taylor <jordantaylor@Jordans-MacBook-Pro-2.local>
2016-07-31 16:52:10 -0700

    Fix bug where user could click to signify completion on already completed task

--- taskssupport.js
+++ taskssupport.js
@@ -7,51 +7,53 @@ if(document.cookie === null){
 	document.cookie = 'expires=Thu, 16-07-17 21:25:05 GMT; tasklist = 0;';
 }
 
+var addtoSS = addTaskRow.bind(null,'SelfStudy')
+$('#SelfStudyButton').click(addtoSS);
+$('#ErrandsButton').click(addTaskRow.bind(null,'Errands'));
+$('#DailyButton').click(addTaskRow.bind(null,'Daily'));
 
-$('.addButton').click(addTaskRow);
-
+refresh()
 });
 
-var addTaskRow = function(){
+var addTaskRow = function(columnName){
 
 	var taskName = prompt("What is the task name?")
-	document.cookie = 'tasklist = ' + get_cookie('tasklist') + '/' + taskName
-
+	document.cookie = 'tasklist = ' + get_cookie('tasklist') + '/' + taskName + "(columnTag:" + columnName+ ")"
 	refresh();
 
 }
 
 var rowClicked = function(){
-	document.cookie = "tasklist = " + get_cookie('tasklist').replace(this.text(),this.text() + " is complete!");
+	if(this.text().indexOf("is complete!") === -1){
+		document.cookie = "tasklist = " + get_cookie('tasklist').replace(this.text(),this.text() + " is complete!");
+	}
 	refresh();
 }
 
 var refresh = function(){
 	$('.Tasks').empty();
 	var taskNames = get_cookie('tasklist').split("/").filter(function(name){return name !== "null"})
-	
 	var UnfinishedTasks = taskNames.filter(function(name){return name.indexOf("is complete!") === -1});
-	console.log(UnfinishedTasks);
 	var FinishedTasks = taskNames.filter(function(name){return UnfinishedTasks.indexOf(name) === -1})
-	console.log(FinishedTasks);
 	UnfinishedTasks.forEach(function(taskname){makeNewRow(taskname)});
 	FinishedTasks.forEach(function(taskname){makeNewRow(taskname)});
 
 	function makeNewRow(taskName){
-		console.log(taskName);
-		var newRow = $('<li>' + taskName +  '</li>')
+		var newRow = $('<li>' + taskName.slice(0,taskName.search("columnTag:") -1) +  '</li>')
+		var columnName = taskName.slice(taskName.search("columnTag:") + 10).replace(")","");
+		
 		var unfinished = taskName.indexOf("is complete!") == -1;
 		if(unfinished){
 			newRow.css("border","thick solid red")
 			newRow.css("background","pink")
-			newRow.css("margin","2.5px")
 		} else {
 			newRow.css("background","aquamarine");
 			newRow.css("border","thick solid green");
 		}
+		newRow.css("margin","5px")
 		newRow.css("box-shadow","10px 10px 5px #888888");
 		newRow.click(rowClicked.bind(newRow));
-		$('.Tasks').append(newRow);
+		$('#' + columnName).append(newRow);
 
 	}
 
